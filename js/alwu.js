b1da06ee6f71f54fe8898bd5239521725af06b7b
SLSRS <slsrs@users.noreply.github.com>
2014-07-16 21:48:10 +0400

    Create alwu.js

---
+++ js/alwu.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+document.getElementById("cf_alert_div").innerHTML = "<div style='padding-left:20px;padding-right:20px;'><br><b>We've got problems!</b> <br> Due to some network error, SLSRS site was not able to load. We've got a cached copy of it. <br><br></div>";
+}
