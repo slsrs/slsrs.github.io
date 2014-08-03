55fa20f78edeb1263d5cd3125a414cd38812660a
SLSRS <slsrs@users.noreply.github.com>
2014-08-03 20:17:00 +0400

    Update makaba_helper.js

--- makaba_helper.js
+++ makaba_helper.js
@@ -1,5 +1,8 @@
 String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+    var makaba_list = ['крым','новороссия', 'еот'];
 $('.thread').each(function(elem,i){
     var item = $(this).attr('id');
-    if($(this).html().toLowerCase().contains('крым') == true) { $(this).fadeOut(); }
+    for(i = 0; i < makaba_list.length; i++) {
+      if($(this).html().toLowerCase().contains(makaba_list[i]) == true) { $(this).fadeOut(); }
+    }
 });
