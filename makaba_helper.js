7a3d7f9bbd47f3c7ee1897b61e71055cb83e01bd
SLSRS <slsrs@users.noreply.github.com>
2014-08-03 20:47:03 +0400

    Update makaba_helper.js

--- makaba_helper.js
+++ makaba_helper.js
@@ -3,9 +3,10 @@ String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 $('.thread').each(function(elem,i){
     var item = $(this).attr('id');
     for(i = 0; i < makaba_list.length; i++) {
+	var id = $(this).attr("id");
+	$("postbtn-hide").attr('onclick', 'mkhelper.hide("'+id+'")');
       if($(this).html().toLowerCase().contains(makaba_list[i]) == true) {
          var thread = $(this).html();
-          var id = $(this).attr("id");
           $(this).html('<div class="mk-hidden" id="mk-'+id+'"><b>Hidden thread: matched</b> '+makaba_list[i]+ '</div> \
           <div style="display:none;" id="mk-full-'+id+'">'+thread+'</div>'); 
           $(this).attr('onclick', 'mkhelper.show("'+id+'")');
@@ -19,5 +20,11 @@ var mkhelper = {
         note.remove();
 		$('#'+id).html(post.html());
 		$('#'+id).attr('onclick','');
-    }
-}
+    },
+	hide: function(id) {
+	var thread = $(this).html();
+    $(this).html('<div class="mk-hidden" id="mk-'+id+'"><b>Hidden thread: hidden by user</b></div> \
+     <div style="display:none;" id="mk-full-'+id+'">'+thread+'</div>'); 
+    $(this).attr('onclick', 'mkhelper.show("'+id+'")');
+	}
+};
