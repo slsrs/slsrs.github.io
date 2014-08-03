47bf9ac0731f2915778d83c807b68f2b367b34a7
SLSRS <slsrs@users.noreply.github.com>
2014-08-03 20:39:00 +0400

    Update makaba_helper.js

--- makaba_helper.js
+++ makaba_helper.js
@@ -4,7 +4,20 @@ $('.thread').each(function(elem,i){
     var item = $(this).attr('id');
     for(i = 0; i < makaba_list.length; i++) {
       if($(this).html().toLowerCase().contains(makaba_list[i]) == true) {
-          $(this).html('<b>Hidden thread: matched</b> '+makaba_list[i]); 
+         var thread = $(this).html();
+          var id = $(this).attr("id");
+          $(this).html('<div class="mk-hidden" id="mk-'+id+'><b>Hidden thread: matched</b> '+makaba_list[i]+ '</div> \
+          <div id="mk-full-'+id+'">'+thread+'</div>'); 
+          $(this).attr('onclick', 'mkhelper.show("id")');
       }
     }
 });
+var mkhelper = {
+    show: function(id) {
+        var note = $("#mk-"+id);
+        var post = $('$mk-full-'+id);
+        note.remove();
+		$('#'+id).html(post.html());
+		$('#'+id).attr('onclick','');
+    }
+}
