a5d958e5490ea7e19e5244e48eb682400580024d
SLSRS <slsrs@users.noreply.github.com>
2014-08-03 20:12:05 +0400

    Update makaba_helper.js

--- makaba_helper.js
+++ makaba_helper.js
@@ -1,3 +1,5 @@
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 $('.thread').each(function(elem,i){
-    var item = $(this).attr('id'); if(document.getElementById(item).innerHTML.search('Росси') == true) { $(this).fadeOut(); }
+    var item = $(this).attr('id');
+    if($(this).html().toLowerCase().contains('крым') == true) { $(this).fadeOut(); }
 });
