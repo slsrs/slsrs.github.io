d9da7f31b51ebd00781e29432ec58893c05eb1b5
SLSRS <slsrs@users.noreply.github.com>
2014-08-07 06:04:10 +0400

    Update alwu.js

--- js/alwu.js
+++ js/alwu.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-document.getElementById("cf_alert_div").innerHTML = "<div style='padding-left:20px;padding-right:20px;'><br><b>We've got problems!</b> <br> Due to some network error, SLSRS site was not able to load. We've got a cached copy of it. <br><br></div>";
+setTimeout("dopopup()", 500);
+}
+function dopopup() {
+  document.getElementById("cf_alert_div").innerHTML = "<div style='padding-left:20px;padding-right:20px;'><br><b>We've got problems!</b> <br> Due to some network error, SLSRS site was not able to load. We've got a cached copy of it. <br><br></div>";
 }
